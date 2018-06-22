f6804ff5d646e051b5a5ce7fed751be65eb4593d
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-21 21:04:59 -0600

    Commit 0

---
+++ js/harryPotter.js
@@ -0,0 +1,6 @@
+console.log("sanity check");
+if (typeof jQuery == 'undefined'){
+	console.log('oops! I still have to link my jQuery properly!');
+} else {
+	console.log('I did it! I linked jQuery and this js file!')
+};

294033b32df4b14e206c05160b70a2af679afe7f
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-21 21:44:02 -0600

    Commit 1 - I added my first element and it was fun

--- js/harryPotter.js
+++ js/harryPotter.js
@@ -1,6 +1,21 @@
-console.log("sanity check");
+
 if (typeof jQuery == 'undefined'){
 	console.log('oops! I still have to link my jQuery properly!');
 } else {
 	console.log('I did it! I linked jQuery and this js file!')
 };
+
+
+
+	const $h1 = $('<h1>').text("Hogwarts");
+		$('body').append($h1);
+
+
+
+// const addText = () => {
+// 	// We want to add a p tag with text to the body
+// 	// 1. create a p tag
+// 	const $p = $('<p/>');
+// 	$p.text('im reall boyyyyyyyy');
+// 	$('body').append($p);
+// }

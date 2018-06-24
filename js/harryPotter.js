edee32cc10699db3440198cf138e1a8b0867fb98
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-24 09:24:25 -0600

    ch.4 save

--- js/harryPotter.js
+++ js/harryPotter.js
@@ -6,29 +6,61 @@ if (typeof jQuery == 'undefined'){
 };
 
 
-
+	const $container = $('#container');
+	
 	const $h1 = $('<h1>').text("Hogwarts");
-		$('body').append($h1);
+		$('div').append($h1);
 
 	const $h2 = $('<h2/>').text("Katie Turner");
-	$('body').append($h2);
+	$('div').append($h2);
 
 	const $h3 = $('<h3/>').text("Gryffindor");
-	$('body').append($h3);
+	$('div').append($h3);
 
 
-	const $h4 = $('<h4/>').text("Leopard");
-	$('body').append($h4);	
-	$('h4').addClass("leopard");
+	const $h4 = $('<h4/>').text("leopard");
+	$('div').append($h4);	
+	$('h4').addClass("pet");
+	$($container).append("<h4>Birch Wand with Phoenix Feather Core</h4>")
+
 	
 	
+	const $ul = $('<ul/>');
+	$('#container').append($ul);
+
+	const $li = $('<li/>').text('butter beer');
+	$('ul').append($li);
+	const $li2 = $('<li2/>').text('invisibilty cloak');
+	$($li2).addClass("secret");
+	$('ul').append($li2);
+	const $li3 = $('<li3/>').text('time turner');
+	$($li3).addClass("secret");
+	$('ul').append($li3);
+	const $li4 = $('<li4/>').text('leash');
+	$($li4).addClass("pet");
+	$('ul').append($li4);
+	const $li5 = $('<li5/>').text('Bertie Botts Every Flavor Beans');
+	$('ul').append($li5);
 
 
+		
+const $table = $('<table></table>');
+const $thead = $('<thead></thead>');
+const $tr = $('<tr></tr>');
+const $th = $('<th></th>');
+const $td = $('<td></td>');
 
-// const addText = () => {
-// 	// We want to add a p tag with text to the body
-// 	// 1. create a p tag
-// 	const $p = $('<p/>');
-// 	$p.text('im reall boyyyyyyyy');
-// 	$('body').append($p);
-// }
+$container.append($table);
+$table.append($thead);
+$thead.append($tr);
+$tr.append('<th>Day</th>');
+$tr.append('<th>Classes</th>');
+$thead.append('<tr></tr>');
+$tr.append('<td>Monday</td>');
+$tr.append('<td>Charms, Quidditch practice, History of Magic</td>');
+$thead.append('<tr></tr>');
+$thead.append('<tr></tr>');
+$thead.append('<tr></tr>');
+$thead.append('<tr></tr>');
+
+
